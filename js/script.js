8f7972c8c61702b376ff68b9b9a9a9ca3b451a6b
ash_c <campbell.ashley735@gmail.com>
2023-07-21 17:51:29 -0500

    Added dropdown menu

--- js/script.js
+++ js/script.js
@@ -1,29 +1,37 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector("#users");
 
-const getData = async function () {
-    const usersRequest = await fetch("https://randomuser.me/api?results=5");
-    const data = await usersRequest.json();
-  
-    const userResults = data.results; // array of objects
-    displayUsers(userResults);
+const getData = async function (numUsers) {
+  const usersRequest = await fetch(
+    `https://randomuser.me/api?results=${numUsers}`
+  );
+  const data = await usersRequest.json();
+
+  const userResults = data.results; // array of objects
+  displayUsers(userResults);
 };
 
 getData();
 
 const displayUsers = function (userResults) {
-    randomFolks.innerHTML = "";
+  // Clear the randomFolks element
+  randomFolks.innerHTML = "";
 
-    for (const user of userResults) {
-        const country = user.location.country;
-        const name = user.name.first;
-        const imageUrl = user.picture.medium;
-        const userDiv = document.createElement("div");
-        userDiv.innerHTML = `
+  for (const user of userResults) {
+    const country = user.location.country;
+    const name = user.name.first;
+    const imageUrl = user.picture.medium;
+    const userDiv = document.createElement("div");
+    userDiv.innerHTML = `
             <h3>${name}</h3>
             <p>${country}</p>
             <img src=${imageUrl} alt="User avatar" />
-            `;
-            randomFolks.append(userDiv);
-    }
+        `;
+    randomFolks.append(userDiv);
+  }
 };
 
+selectUserNumber.addEventListener("change", function (e) {
+  const numUsers = e.target.value;
+  getData(numUsers);
+});
